56b5ad8dee21ac38ad71d1a05c9f1d693e00643a
James Orson <jamesaorson@gmail.com>
2023-03-21 20:33:43 +0000

    Disable raw mode and enable at exit

---
+++ src/c/raw.c
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios _ded_original_termios;
+
+/*
+  DOC: Prevents the echoing of characters in the terminal when the user types.
+  This allows us to fully manipulate the printing of characters given to ded.
+*/
+void ded_raw_enable();
+/*
+  DOC: Returns the terminal back to the mode where characters are echoed back
+*/
+void _ded_raw_disable();
+
+void _ded_raw_disable() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &_ded_original_termios);
+}
+
+void ded_raw_enable() {
+  tcgetattr(STDIN_FILENO, &_ded_original_termios);
+  atexit(_ded_raw_disable);
+
+  struct termios raw;
+  tcgetattr(STDIN_FILENO, &raw);
+  raw.c_lflag &= ~(ECHO);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
