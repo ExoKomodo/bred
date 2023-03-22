c06da4253a7b09e02bb2783edbcf12c7398f877c
James Orson <jamesaorson@gmail.com>
2023-03-22 05:47:45 +0000

    Fix segmentation fault

--- src/include/helpers.h
+++ src/include/helpers.h
@@ -6,36 +6,37 @@
 #include <termios.h>
 #include <unistd.h>
 
-int bred_dead(EditorConfig, const char*, int);
+static termios _original_terminal_data;
+
+void bred_dead(const char*, int);
 /*
   DOC: Returns the terminal back to the mode where characters are echoed back
 */
-void _bred_raw_disable(EditorConfig);
+void _bred_raw_disable();
 /*
   DOC: Prevents the echoing of characters in the terminal when the user types.
   This allows us to fully manipulate the printing of characters given to bred.
 */
 EditorConfig bred_raw_enable(EditorConfig);
 
-int bred_dead(EditorConfig editor_config, const char* err, int code) {
+void bred_dead(const char* err, int code) {
   perror(err);
-  bred_raw_disable(editor_config);
   exit(code);
-  return code;
 }
 
-void bred_raw_disable(EditorConfig editor_config) {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, editor_config.original_terminal_data) == -1) {
-    bred_dead(editor_config, "tcsetattr", 1);
+void _bred_raw_disable() {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &_original_terminal_data) == -1) {
+    bred_dead("tcsetattr", 1);
   }
 }
 
 EditorConfig bred_raw_enable(EditorConfig editor_config) {
-  if (tcgetattr(STDIN_FILENO, editor_config.original_terminal_data) == -1) {
-    bred_dead(editor_config, "tcgetattr", 1);
+  if (tcgetattr(STDIN_FILENO, &_original_terminal_data) == -1) {
+    bred_dead("tcgetattr", 1);
   }
+  atexit(_bred_raw_disable);
 
-  editor_config.terminal_data = editor_config.original_terminal_data;
+  editor_config.terminal_data = &_original_terminal_data;
   tcgetattr(STDIN_FILENO, editor_config.terminal_data);
   // BRKINT: When BRKINT is turned on, a break condition will cause a SIGINT signal to be sent to the program, like pressing CTRL+c
   // ICRNL: Translate CTRL+m and enter key into carriage return
@@ -63,7 +64,7 @@ EditorConfig bred_raw_enable(EditorConfig editor_config) {
   // Unit is 1/10 of a second
   editor_config.terminal_data->c_cc[VTIME] = 1;
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, editor_config.terminal_data) == -1) {
-    bred_dead(editor_config, "tcsetattr", 1);
+    bred_dead("tcsetattr", 1);
   }
 
   return editor_config;
--- src/include/types.h
+++ src/include/types.h
@@ -3,7 +3,11 @@
 typedef struct termios termios;
 struct EditorConfig {
   char* input;
-  termios* original_terminal_data;
   termios* terminal_data;
 };
 typedef struct EditorConfig EditorConfig;
+int empty_editor_config(EditorConfig editor_config) {
+  char* input = editor_config.input;
+  free(input);
+  return 0;
+}
