d4f4f368ec48f884086a549a0320460604e27750
James Orson <jamesaorson@gmail.com>
2023-03-24 21:41:34 +0000

    Minimize the C code used

--- src/include/raw.h
+++ src/include/raw.h
@@ -8,7 +8,6 @@
 
 static termios _original_terminal_data;
 
-int bred_dead(const char*, int);
 /*
   DOC: Returns the terminal back to the mode where characters are echoed back
 */
@@ -17,15 +16,7 @@ void _bred_raw_disable();
   DOC: Prevents the echoing of characters in the terminal when the user types.
   This allows us to fully manipulate the printing of characters given to bred.
 */
-EditorConfig bred_raw_enable(EditorConfig);
-
-int bred_dead(const char* err, int code) {
-  // Implicitly imported in main.day
-  bred_clear_screen();
-  perror(err);
-  exit(code);
-  return code;
-}
+BreditorConfig bred_raw_enable(BreditorConfig);
 
 void _bred_raw_disable() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &_original_terminal_data) == -1) {
@@ -33,16 +24,16 @@ void _bred_raw_disable() {
   }
 }
 
-EditorConfig bred_raw_enable(EditorConfig editor_config) {
+BreditorConfig bred_raw_enable(BreditorConfig config) {
   if (tcgetattr(STDIN_FILENO, &_original_terminal_data) == -1) {
     bred_dead("tcgetattr", 1);
   }
   atexit(_bred_raw_disable);
 
-  editor_config.terminal_data = malloc(sizeof(termios));
-  *(editor_config.terminal_data) = _original_terminal_data;
+  config.terminal_data = malloc(sizeof(termios));
+  *(config.terminal_data) = _original_terminal_data;
 
-  tcgetattr(STDIN_FILENO, editor_config.terminal_data);
+  tcgetattr(STDIN_FILENO, config.terminal_data);
   // BRKINT: When BRKINT is turned on, a break condition will cause a SIGINT signal to be sent to the program, like pressing CTRL+c
   // ICRNL: Translate CTRL+m and enter key into carriage return
   // INPCK: Enables parity checking, which doesn’t seem to apply to modern terminal emulators
@@ -51,26 +42,26 @@ EditorConfig bred_raw_enable(EditorConfig editor_config) {
     This was probably already turned off
   */
   // IXON: Ignore CTRL+s suspend and CTRL+q resume
-  editor_config.terminal_data->c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  config.terminal_data->c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   // OPOST: Turns off output processing that translates newlines to carriage returns
-  editor_config.terminal_data->c_oflag &= ~(OPOST);
+  config.terminal_data->c_oflag &= ~(OPOST);
   /*
     CS8: not a flag, it is a bit mask with multiple bits,
     which we set using the bitwise-OR (|) operator unlike all the flags we are turning off.
     It sets the character size (CS) to 8 bits per byte.
   */
-  editor_config.terminal_data->c_cflag |= (CS8);
+  config.terminal_data->c_cflag |= (CS8);
   // ECHO: Ignore echo of characters
   // ICANON: Ignore enter key press requirement for input
   // IEXTEN: Ignore CTRL+v waiting for a character to type literally
   // ISIG: Ignore CTRL+c cancel and CTRL+z background
-  editor_config.terminal_data->c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-  editor_config.terminal_data->c_cc[VMIN] = 0;
+  config.terminal_data->c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  config.terminal_data->c_cc[VMIN] = 0;
   // Unit is 1/10 of a second
-  editor_config.terminal_data->c_cc[VTIME] = 1;
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, editor_config.terminal_data) == -1) {
+  config.terminal_data->c_cc[VTIME] = 1;
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, config.terminal_data) == -1) {
     bred_dead("tcsetattr", 1);
   }
 
-  return editor_config;
+  return config;
 }
--- src/include/types.h
+++ src/include/types.h
@@ -1,18 +1,7 @@
 #pragma once
 
 #include <stdlib.h>
+#include <sys/ioctl.h>
 
 typedef struct termios termios;
-struct EditorConfig {
-  char* input;
-  termios* terminal_data;
-  size_t number_of_rows;
-};
-typedef struct EditorConfig EditorConfig;
-int empty_editor_config(EditorConfig editor_config) {
-  char* input = editor_config.input;
-  free(input);
-  termios* terminal_data = editor_config.terminal_data;
-  free(terminal_data);
-  return 0;
-}
+typedef struct winsize winsize;
